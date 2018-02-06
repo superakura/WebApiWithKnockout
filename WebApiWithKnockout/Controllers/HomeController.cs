88755178002debb8a547eb889906b446a8472065
superakura <superakura@hotmail.com>
2018-02-06 13:12:46 +0800

    项目初始化，使用传统MVC模式

---
+++ WebApiWithKnockout/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApiWithKnockout.Models;
+
+namespace WebApiWithKnockout.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ReservationRepository repo = new ReservationRepository();
+
+        public ViewResult Index()
+        {
+            return View(repo.GetAll());
+        }
+
+        public ActionResult Add(Reservation item)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.Add(item);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+
+        public ActionResult Remove(int id)
+        {
+            repo.Remove(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update(Reservation item)
+        {
+            if (ModelState.IsValid&&repo.Update(item))
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ WebApiWithKnockout/Models/ReservationRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiWithKnockout.Models
+{
+    public class ReservationRepository
+    {
+        private static ReservationRepository repo = new ReservationRepository();
+
+        public static ReservationRepository Current
+        {
+            get
+            {
+                return repo;
+            }
+        }
+
+        private List<Reservation> data = new List<Reservation>
+        {
+            new Reservation {ReservationId=1,ClientName="zm",Location="usa" },
+            new Reservation {ReservationId=1,ClientName="ys",Location="china" },
+            new Reservation {ReservationId=1,ClientName="xtz",Location="japan" }
+        };
+
+        public IEnumerable<Reservation> GetAll()
+        {
+            return data;
+        }
+
+        public Reservation Get(int id)
+        {
+            return data.Where(w => w.ReservationId == id).FirstOrDefault();
+        }
+
+        public Reservation Add(Reservation item)
+        {
+            item.ReservationId = data.Count + 1;
+            data.Add(item);
+            return item;
+        }
+
+        public void Remove(int id)
+        {
+            Reservation item = Get(id);
+            if(item!=null)
+            {
+                data.Remove(item);
+            }
+        }
+
+        public bool Update(Reservation item)
+        {
+            Reservation storedItem = Get(item.ReservationId);
+            if (storedItem!=null)
+            {
+                storedItem.ClientName = item.ClientName;
+                storedItem.Location = item.Location;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
